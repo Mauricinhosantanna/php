0032dbf204420576f57811d4858d0d3a7a593bca
Mauricinhosantanna <mauricinhosantanna@hotmail.com>
2023-09-20 11:41:04 -0300

    resolvi  o conflito

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-echo "Meu primeiro codigo php! <strong >Ola Mundo456</strong>";
-echo "ola udemy"
 
-=======
-echo "Meu primeiro codigo php! <strong >Ola Mundo123</strong>";
-echo "olha Hcode"
->>>>>>> bcc9b5a3ef33b50616c26dada7d0b2f6a10f2e92
+echo "Meu primeiro codigo php! <strong >Ola Mundo456</strong>";
+echo "ola udemy e ola hcode"
+
 ?>
